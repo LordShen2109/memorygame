1a9726149f966df4126354e82ae841ee870f7a8d
SakshamTalwar12 <talwarsaksham125@gmail.com>
2024-11-17 00:42:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
+  let flippedCards = [];
+
   $(".card").click(function() {
-    $(this).toggleClass("flipped");
+    if ($(this).hasClass("flipped") || flippedCards.length >= 2) {
+      return; // Prevent flipping more than two cards or flipping an already flipped card
+    }
+
+    $(this).addClass("flipped");
+    flippedCards.push($(this));
+
+    if (flippedCards.length === 2) {
+      // Check if the two flipped cards match
+      const card1 = flippedCards[0];
+      const card2 = flippedCards[1];
+
+      if (card1.data("match") === card2.data("match")) {
+        // Cards match: hide them
+        setTimeout(() => {
+          card1.addClass("hidden");
+          card2.addClass("hidden");
+          flippedCards = [];
+        }, 500); // Allow the flip animation to complete
+      } else {
+        // Cards do not match: flip them back
+        setTimeout(() => {
+          card1.removeClass("flipped");
+          card2.removeClass("flipped");
+          flippedCards = [];
+        }, 1000); // Allow the user to see the unmatched cards briefly
+      }
+    }
   });
 });
+
